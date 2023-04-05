27432fa5fe3b43512c414272cc530da4d911a563
Daniel Thorne <thorne.danielj@gmail.com>
2023-04-04 20:07:20 -0600

    PhoneOrEmptyAttribute

---
+++ DJT.ComponentModel/DataAnnotations/PhoneOrEmptyAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJT.ComponentModel.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public class PhoneOrEmptyAttribute : DataTypeAttribute
+    {
+        public PhoneOrEmptyAttribute()
+            : base(DataType.PhoneNumber)
+        {
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (!(value is string valueAsString))
+            {
+                return false;
+            }
+
+            if (valueAsString == string.Empty)
+            {
+                return true;
+            }
+
+            PhoneAttribute phoneAttribute = new();
+            return phoneAttribute.IsValid(value);
+        }
+        
+    }
+}
