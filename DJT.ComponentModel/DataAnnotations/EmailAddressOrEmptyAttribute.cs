11fa248dee11993e079fa964412b8f188ace065c
Daniel Thorne <thorne.danielj@gmail.com>
2023-04-04 20:07:10 -0600

    EmailAddressOrEmptyAttribute

---
+++ DJT.ComponentModel/DataAnnotations/EmailAddressOrEmptyAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJT.ComponentModel.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public class EmailAddressOrEmptyAttribute : DataTypeAttribute
+    {
+        public EmailAddressOrEmptyAttribute() : base(DataType.EmailAddress)
+        {
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (!(value is string valueAsString))
+            {
+                return false;
+            }
+
+            if (valueAsString == string.Empty)
+            {
+                return true;
+            }
+
+            return (new EmailAddressAttribute().IsValid(value));
+        }
+    }
+}
